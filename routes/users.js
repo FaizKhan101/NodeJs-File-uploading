c8576016275a2d7b48c6d2796b5df02d31bbc060
Faiz <faizkhan8225@gmail.com>
2024-02-20 15:20:23 +0530

    Complete file uploading

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,42 @@
-const express = require('express');
+const express = require("express");
+const multer = require("multer");
+
+const db = require("../data/database");
+
+const fileStore = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: fileStore });
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray()
+  res.render("profiles", { users });
 });
 
-router.get('/new-user', function(req, res) {
-  res.render('new-user');
+router.get("/new-user", function (req, res) {
+  res.render("new-user");
+});
+
+router.post("/profiles", upload.single("image"), async (req, res, next) => {
+  const uploadedImageFile = req.file;
+  const uploadedData = req.body;
+
+  await db.getDb().collection("users").insertOne({
+    name: uploadedData.username,
+    imagePath: uploadedImageFile.path,
+  });
+
+  console.log(uploadedData);
+  console.log(uploadedImageFile);
+  res.redirect("/");
 });
 
 module.exports = router;
